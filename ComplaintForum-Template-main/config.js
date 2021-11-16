90d30e7cf759ddb70c13d70e1bf1067d02792e35
DRACOCRYPT <77237822+DRACOCRYPT@users.noreply.github.com>
2021-11-16 17:02:26 +0530

    Add files via upload

---
+++ ComplaintForum-Template-main/config.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase'
+require("@firebase/firestore")
+
+const firebaseConfig = {
+ //Add configuration here
+ const firebaseConfig = {
+  apiKey: "AIzaSyCYO542Zk7L22kFmLJ9sOWeEEDzvW83dvk",
+  authDomain: "complaintform-f52a0.firebaseapp.com",
+  projectId: "complaintform-f52a0",
+  storageBucket: "complaintform-f52a0.appspot.com",
+  messagingSenderId: "343104565917",
+  appId: "1:343104565917:web:207e18b79d4a89b6791a74"
+};
+};
+  firebase.initializeApp(firebaseConfig);
+
+  export default firebase.firestore()
+
